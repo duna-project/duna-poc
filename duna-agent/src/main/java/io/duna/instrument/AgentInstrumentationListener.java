423b24c505b51682865d158dc74a857da382d0e8
ceduardo.melo <ceduardo.melo@gmail.com>
2016-12-08 10:24:38 -0200

    downgrade kotlin version to 1.0.5

--- duna-agent/src/main/java/io/duna/instrument/AgentInstrumentationListener.java
+++ duna-agent/src/main/java/io/duna/instrument/AgentInstrumentationListener.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2016 Duna Project
+ * Ministério do Planejamento, Desenvolvimento de Gestão
+ * República Federativa do Brasil
+ *
+ * This file is part of the Duna Project.
+ */
 package io.duna.instrument;
 
 import io.duna.agent.DunaJavaAgent;
--- duna-core/src/main/kotlin/io/duna/core/io/BufferInputStream.kt
+++ duna-core/src/main/kotlin/io/duna/core/io/BufferInputStream.kt
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2016 Duna Project
+ * Ministério do Planejamento, Desenvolvimento de Gestão
+ * República Federativa do Brasil
+ *
+ * This file is part of the Duna Project.
+ */
 package io.duna.core.io
 
 import io.vertx.core.buffer.Buffer
